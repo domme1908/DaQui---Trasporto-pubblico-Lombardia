bfa6a4ce877b59ffe587977e1e0fa858fb5c8431
Dominik - ge54set <dominik.markart@tum.de>
2022-03-11 21:44:21 +0100

    First commit

---
+++ varese_transport/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//Main colors
+const kPrimaryColor = Color(0x22577A);
+const kSecondaryColor = Color(0x38A3A5);
+const kBackgroundColor = Color(0xC7F9CC);
+const kTextColor = Color(0x0A1923);
+
+//Padding
+const double kDefaultPadding = 20.0;

78c6065af36f99d15044f81d6c36f3306dce1568
Dominik - ge54set <dominik.markart@tum.de>
2022-04-08 19:51:00 +0200

    Alpha 1.10 Map works

--- varese_transport/lib/screens/details/components/map.dart
+++ varese_transport/lib/screens/details/components/map.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
+import 'package:varese_transport/lib/classes/section.dart';
+import 'package:varese_transport/lib/classes/stop.dart';
 
 import '../../../lib/classes/itinerary.dart';
 import '../details_screen.dart';
@@ -79,45 +81,128 @@ class _OSMapState extends State<OSMap> {
     return SfMaps(
       layers: [
         MapTileLayer(
-          zoomPanBehavior: _zoomPanBehavior,
-          controller: mapController,
-          initialFocalLatLng: MapLatLng(45.359277, 8.798542),
-          //initialZoomLevel: getInitialZoomeBasedOnDistance(),
-          initialLatLngBounds: MapLatLngBounds(
-              MapLatLng(min(latArr, latDep) - getDistance() / 90000,
-                  max(longArr, longDep) + getDistance() / 1000000),
-              MapLatLng(max(latArr, latDep) + getDistance() / 300000,
-                  min(longArr, longDep) - getDistance() / 1000000)),
-          urlTemplate:
-              'https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=VBw9do6eEQAZXKn5YhfG',
-          sublayers: [
-            MapPolylineLayer(
-              polylines: Set.of([
-                MapPolyline(
-                  points: polylinePoints,
-                )
-              ]),
-            ),
-          ],
-          initialMarkersCount: 2,
-          markerBuilder: (context, index) {
-            if (index == 0) {
-              return MapMarker(
-                  iconColor: Colors.white,
-                  iconStrokeColor: Colors.blue,
-                  iconStrokeWidth: 2,
-                  latitude: polylinePoints[index].latitude,
-                  longitude: polylinePoints[index].longitude);
-            }
-            return MapMarker(
-                iconColor: Colors.white,
-                iconStrokeColor: Colors.blue,
-                iconStrokeWidth: 2,
-                latitude: polylinePoints[polylinePoints.length - 1].latitude,
-                longitude: polylinePoints[polylinePoints.length - 1].longitude);
-          },
-        ),
+            zoomPanBehavior: _zoomPanBehavior,
+            controller: mapController,
+            initialFocalLatLng: MapLatLng(45.359277, 8.798542),
+            //initialZoomLevel: getInitialZoomeBasedOnDistance(),
+            initialLatLngBounds: MapLatLngBounds(
+                MapLatLng(min(latArr, latDep) - getDistance() / 90000,
+                    max(longArr, longDep) + getDistance() / 1000000),
+                MapLatLng(max(latArr, latDep) + getDistance() / 300000,
+                    min(longArr, longDep) - getDistance() / 1000000)),
+            urlTemplate:
+                'https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=VBw9do6eEQAZXKn5YhfG',
+            sublayers: [
+              MapPolylineLayer(polylines: getLines().toSet()),
+            ],
+            initialMarkersCount: getListOfMarkers().length,
+            markerBuilder: (BuildContext context, int index) {
+              return getMarkers(context, index);
+            }),
       ],
     );
+  }
+
+  List<MapPolyline> getLines() {
+    List<MapPolyline> result = [];
+    for (Section section in chosenSolution.sections) {
+      MapPolyline sectionLine;
+      List<MapLatLng> points = [];
+      if (section.stops.length == 0) {
+        points = [
+          MapLatLng(double.parse(section.yDeparture),
+              double.parse(section.xDeparture)),
+          MapLatLng(
+              double.parse(section.yArrival), double.parse(section.xArrival))
+        ];
+        sectionLine = MapPolyline(points: points, color: Colors.blue);
+        result.add(sectionLine);
+      } else {
+        for (Stop stop in section.stops) {
+          print(section.line);
+          points.add(MapLatLng(double.parse(stop.y), double.parse(stop.x)));
+          sectionLine = MapPolyline(
+              points: points,
+              color: Colors.primaries.elementAt(
+                  getIntFromString(section.line) % Colors.primaries.length));
+          result.add(sectionLine);
+        }
+      }
+    }
+    return result;
+  }
+
+  int getIntFromString(String lineName) {
+    var result = 0;
+    for (var i = 0; i < lineName.length; i++) {
+      result += lineName.codeUnitAt(i);
+    }
+
+    return result;
+  }
+
+  List<MapMarker> getListOfMarkers() {
+    List<MapMarker> result = [];
+    Section section;
+    Stop stop;
+    for (var i = 0; i < chosenSolution.sections.length; i++) {
+      section = chosenSolution.sections.elementAt(i);
+      if (i == 0) {
+        result.add(MapMarker(
+            child: Icon(
+              Icons.circle,
+              color: Colors.yellow,
+            ),
+            latitude: double.parse(section.yDeparture),
+            longitude: double.parse(section.xDeparture)));
+      } else if (i == chosenSolution.sections.length - 1) {
+        result.add(MapMarker(
+            child: Icon(
+              Icons.circle,
+              color: Colors.blue,
+            ),
+            latitude: double.parse(section.yDeparture),
+            longitude: double.parse(section.xDeparture)));
+      } else {
+        if (section.stops == 0) {
+          result.add(MapMarker(
+              child: Icon(
+                Icons.circle,
+                color: Colors.red,
+              ),
+              latitude: double.parse(section.yDeparture),
+              longitude: double.parse(section.xDeparture)));
+        } else {
+          for (var j = 0; j < section.stops.length; j++) {
+            stop = section.stops.elementAt(j);
+            if (j == 0) {
+              result.add(MapMarker(
+                  child: Icon(
+                    Icons.circle,
+                    color: Colors.red,
+                    size: 15,
+                  ),
+                  latitude: double.parse(stop.y),
+                  longitude: double.parse(stop.x)));
+            } else {
+              result.add(MapMarker(
+                  child: Icon(
+                    Icons.circle,
+                    color: Colors.green,
+                    size: 10,
+                  ),
+                  latitude: double.parse(stop.y),
+                  longitude: double.parse(stop.x)));
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  MapMarker getMarkers(BuildContext context, int index) {
+    List<MapMarker> result = getListOfMarkers();
+    return result.elementAt(index);
   }
 }
